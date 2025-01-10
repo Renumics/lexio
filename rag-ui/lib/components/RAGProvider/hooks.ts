3f1b6a8735e647dc4e8990ad33f004f5562bb584
Daniel Klitzke <daniel.klitzke@renumics.com>
2025-01-10 17:07:45 +0100

    begin more hook functions

--- rag-ui/lib/components/RAGProvider/hooks.ts
+++ rag-ui/lib/components/RAGProvider/hooks.ts
@@ -1,15 +1,17 @@
-import { completedMessagesAtom, loadingAtom, currentStreamAtom } from '../../state/rag-state';
-import { useAtom } from 'jotai';
+import { completedMessagesAtom, loadingAtom, currentStreamAtom, addMessageAtom } from '../../state/rag-state';
+import { useAtom, useSetAtom } from 'jotai';
 
 export const useRAG = () => {
 
   // Get state from atoms
+  const addMessage = useSetAtom(addMessageAtom);
   const [messages] = useAtom(completedMessagesAtom);
   const [currentStream] = useAtom(currentStreamAtom);
   const [loading] = useAtom(loadingAtom);
 
   return {
     // State
+    addMessage,
     messages,
     currentStream,
     loading
