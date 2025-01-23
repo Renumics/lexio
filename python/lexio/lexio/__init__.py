6242f42c78848b5ca93b0b32efb729566cec1501
Marius Steger <marius000steger@gmail.com>
2025-01-23 15:51:31 +0100

    merged current main into branch

--- python/lexio/lexio/__init__.py
+++ python/lexio/lexio/__init__.py
@@ -4,7 +4,7 @@ This file is auto-generated. Do not edit directly.
 
 Generated from:
   source:    lexio.types
-  timestamp: 2025-01-23T14:44:13+00:00
+  timestamp: 2025-01-23T15:49:56+00:00
 """
 
 from lexio.types import (
