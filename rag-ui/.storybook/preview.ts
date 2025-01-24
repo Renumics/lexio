36963c45245f7f8aa845b2fb319dc0e04e3e047e
Markus Stoll <markus.stoll@renumics.com>
2025-01-24 08:29:39 +0100

    themed and more categories

--- rag-ui/.storybook/preview.ts
+++ rag-ui/.storybook/preview.ts
@@ -2,6 +2,11 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    options: {
+      storySort: {
+        order: ['Configure your project', 'Welcome', 'Components'],
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
