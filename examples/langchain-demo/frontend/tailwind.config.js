5236f8e21086987b3dad2926598d17271f9ffe1e
Daniel Klitzke <daniel.klitzke@renumics.com>
2025-01-29 14:56:10 +0100

    rename rag-ui to lexio

--- examples/langchain-demo/frontend/tailwind.config.js
+++ examples/langchain-demo/frontend/tailwind.config.js
@@ -3,8 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "../../../../rag-ui/lib/**/*.{js,ts,jsx,tsx}",
-    "../../../../rag-ui/src/**/*.{js,ts,jsx,tsx}"
+    "../../../../lexio/lib/**/*.{js,ts,jsx,tsx}",
+    "../../../../lexio/src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
