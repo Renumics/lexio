172b47f99427c3ddb2bf040861f2d703299a0dc5
Marius Steger <marius000steger@gmail.com>
2025-03-05 15:11:05 +0100

    WIP on feature/docu-new-api

---
+++ lexio/.prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  useTabs: false
+}; 
---
+++ lexio/.prettierrc.js
@@ -0,0 +1,13 @@
+export default {
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  useTabs: false
+}; 
